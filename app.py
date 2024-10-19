62b84e87cfc380386a7dc4d630749b72c4b8f1aa
Denurta Dimurtianti Putri <159613788+Denurta@users.noreply.github.com>
2024-10-19 15:11:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,106 +14,56 @@ st.set_page_config(page_title="Prediksi Saham LQ45", layout="wide", initial_side
 def add_custom_css():
     st.markdown("""
         <style>
-        /* Change background color and font */
         body {
             background-color: #F5F5F5;
             font-family: 'Arial', sans-serif;
         }
-
-        /* Style the main title */
         .stApp header {
-            background: #4682B4; /* Steel Blue */
+            background: #4682B4; 
             padding: 10px;
         }
-
-        /* Style the sidebar */
         .sidebar-content {
-            background-color: #4682B4; /* Steel Blue */
+            background-color: #4682B4;
             color: white;
         }
-
-        /* Style buttons */
         .stButton>button {
-            background-color: #4682B4; /* Steel Blue */
+            background-color: #4682B4;
             color: white;
             border-radius: 12px;
         }
-
-        /* Style sliders */
         .stSlider label {
-            color: #4682B4; /* Steel Blue */
+            color: #4682B4;
         }
-
-        /* Add spacing between sections */
         .stMarkdown {
             margin-top: 20px;
             margin-bottom: 20px;
         }
-
-        /* Styling table */
         .dataframe {
-            border: 1px solid #4682B4; /* Steel Blue */
+            border: 1px solid #4682B4;
             border-radius: 5px;
         }
-
-        /* Make images responsive */
         img {
             max-width: 100%;
             height: auto;
         }
-
         </style>
-        """, unsafe_allow_html=True)
-
-# Function to display the title with a logo in a visually appealing way
-def display_title_with_logo():
-    st.markdown("""
-        <style>
-        .title-container {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-bottom: 30px;
-        }
-
-        .title-container img {
-            max-width: 80px;
-            margin-right: 20px;
-        }
-
-        .title-container h1 {
-            font-size: 3rem;
-            color: #4682B4;
-            margin: 0;
-        }
-
-        .stApp header {
-            background-color: #F5F5F5; /* Light background */
-        }
-        </style>
-        <div class="title-container">
-            <img src="https://kuliahdimana.id/public/magang/22954b52c576f28055eac57190504a20.jpg" alt="Logo">
-            <h1>Prediksi Saham LQ45</h1>
-        </div>
         """, unsafe_allow_html=True)
 
 # Function to select dynamic lags based on autocorrelation
 def select_dynamic_lags(price_data, max_lags=20, threshold=0.2):
     autocorr_values = acf(price_data, nlags=max_lags)
     significant_lags = [lag for lag, value in enumerate(autocorr_values[1:], start=1) if abs(value) > threshold]
-    if not significant_lags:
-        significant_lags = [1, 5, 10]
-    return significant_lags
+    return significant_lags if significant_lags else [1, 5, 10]
 
 # List of LQ45 stock tickers on Yahoo Finance
 lq45_tickers = [
-    'ACES.JK', 'ADRO.JK', 'AKRA.JK', 'AMMN.JK', 'AMRT.JK', 'ANTM.JK', 
-    'ARTO.JK', 'ASII.JK', 'BBCA.JK', 'BBNI.JK', 'BBRI.JK', 'BBTN.JK', 
-    'BMRI.JK', 'BRIS.JK', 'BRPT.JK', 'BUKA.JK', 'CPIN.JK', 'ESSA.JK', 
-    'EXCL.JK', 'GGRM.JK', 'GOTO.JK', 'HRUM.JK', 'ICBP.JK', 'INCO.JK', 
-    'INDF.JK', 'INKP.JK', 'INTP.JK', 'ISAT.JK', 'ITMG.JK', 'JSMR.JK', 
-    'KLBF.JK', 'MAPI.JK', 'MBMA.JK', 'MDKA.JK', 'MEDC.JK', 'MTEL.JK', 
-    'PGAS.JK', 'PGEO.JK', 'PTBA.JK', 'SIDO.JK', 'SMGR.JK', 'TLKM.JK', 
+    'ACES.JK', 'ADRO.JK', 'AKRA.JK', 'AMMN.JK', 'AMRT.JK', 'ANTM.JK',
+    'ARTO.JK', 'ASII.JK', 'BBCA.JK', 'BBNI.JK', 'BBRI.JK', 'BBTN.JK',
+    'BMRI.JK', 'BRIS.JK', 'BRPT.JK', 'BUKA.JK', 'CPIN.JK', 'ESSA.JK',
+    'EXCL.JK', 'GGRM.JK', 'GOTO.JK', 'HRUM.JK', 'ICBP.JK', 'INCO.JK',
+    'INDF.JK', 'INKP.JK', 'INTP.JK', 'ISAT.JK', 'ITMG.JK', 'JSMR.JK',
+    'KLBF.JK', 'MAPI.JK', 'MBMA.JK', 'MDKA.JK', 'MEDC.JK', 'MTEL.JK',
+    'PGAS.JK', 'PGEO.JK', 'PTBA.JK', 'SIDO.JK', 'SMGR.JK', 'TLKM.JK',
     'TOWR.JK', 'UNTR.JK', 'UNVR.JK'
 ]
 
@@ -122,7 +72,7 @@ def main():
     add_custom_css()
 
     # Sidebar navigation
-    page = st.sidebar.selectbox("Pilih Halaman", ["Prediksi Saham", "Penjelasan"])
+    page = st.sidebar.selectbox("Pilih Halaman", ["Prediksi Saham", "Penjelasan Metrik dan Threshold"])
 
     if page == "Prediksi Saham":
         display_title_with_logo()
@@ -145,12 +95,12 @@ def show_prediction_page():
     forecast_days = st.sidebar.slider('Jumlah Hari untuk Ramalan:', 1, 30, 1, format="%d hari")
 
     # Slider for threshold for autocorrelation
-    threshold = st.sidebar.slider('Threshold:', 0.0, 1.0, 0.00, step=0.01, format="%.2f")
+    threshold = st.sidebar.slider('Threshold Autokorelasi:', 0.0, 1.0, 0.2, step=0.01, format="%.2f")
 
     if selected_stock != 'Pilih Saham':
         stock_data = get_stock_data(selected_stock, start_date, end_date)
         st.write(f"<b>Data Harga Penutupan {selected_stock}:</b>", unsafe_allow_html=True)
-        st.write(stock_data)
+        st.dataframe(stock_data)
 
         dynamic_lags = select_dynamic_lags(stock_data['Close'], max_lags=20, threshold=threshold)
 
@@ -161,7 +111,7 @@ def show_prediction_page():
             # Create Plot
             fig = go.Figure()
             fig.add_trace(go.Scatter(x=y_test.index, y=y_test, mode='lines', name='Harga Aktual'))
-            fig.add_trace(go.Scatter(x=y_test.index, y=y_pred, mode='lines', name='Harga Prediksi')))
+            fig.add_trace(go.Scatter(x=y_test.index, y=y_pred, mode='lines', name='Harga Prediksi', line=dict(dash='dash')))
             future_dates = pd.date_range(y_test.index[-1] + pd.Timedelta(days=1), periods=forecast_days)
             fig.add_trace(go.Scatter(x=future_dates, y=future_preds, mode='lines', name='Prediksi Masa Depan', line=dict(dash='dot', color='green')))
             fig.update_layout(title=f'Prediksi Harga Saham {selected_stock} dan Ramalan {forecast_days} Hari', xaxis_title='Tanggal', yaxis_title='Harga Penutupan')
@@ -186,53 +136,47 @@ def show_explanation_page():
     
     - **R² (R-squared)**: R² mengukur seberapa baik model menjelaskan variasi data. Nilainya berkisar dari 0 hingga 1. Semakin mendekati 1, semakin baik model menjelaskan data.
     
-    - **Mean Absolute Percentage Error (MAPE)**: MAPE menunjukkan persentase kesalahan rata-rata dalam prediksi. Semakin kecil nilainya, semakin akurat prediksinya.
-    
     ### Penjelasan Threshold:
     
-    - **Threshold Autokorelasi**: Threshold digunakan untuk menentukan seberapa kuat hubungan antara nilai saat ini dengan nilai sebelumnya dalam data. Semakin tinggi threshold, semakin ketat model dalam memilih lag yang signifikan. Contoh: Jika threshold diatur ke 0.2, hanya lag dengan autokorelasi lebih dari 0.2 yang dianggap signifikan.
+    - **Threshold Autokorelasi**: Threshold digunakan untuk menentukan seberapa kuat hubungan antara nilai saat ini dengan nilai sebelumnya dalam data. Semakin tinggi threshold, semakin ketat model dalam memilih lag yang signifikan. Contoh: Jika threshold diatur ke 0.2, hanya lag dengan autokorelasi lebih dari 0.2 yang akan digunakan.
     """)
 
-# Function to get stock data using Yahoo Finance
+# Function to fetch stock data from Yahoo Finance with caching
+@st.cache_data
 def get_stock_data(ticker, start, end):
-    stock_data = yf.download(ticker, start=start, end=end)
-    return stock_data
+    stock = yf.Ticker(ticker)
+    data = stock.history(start=start, end=end)
+    return data[['Close']]
 
-# Function to perform XGBoost forecasting
-def xgboost_forecast(stock_data, forecast_days, dynamic_lags):
-    # Prepare data for forecasting (using XGBoost)
-    X = []
-    y = []
-    close_prices = stock_data['Close'].values
-    for i in range(max(dynamic_lags), len(close_prices)):
-        X.append([close_prices[i - lag] for lag in dynamic_lags])
-        y.append(close_prices[i])
-    
-    X = np.array(X)
-    y = np.array(y)
-    
-    # Train/Test Split
-    split_index = len(X) - forecast_days
-    X_train, X_test = X[:split_index], X[split_index:]
-    y_train, y_test = y[:split_index], y[split_index:]
-    
-    # Create and train XGBoost model
-    model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100)
+# Function for forecasting using XGBoost
+def xgboost_forecast(data, forecast_days, dynamic_lags):
+    price_data = data['Close']
+    lags = dynamic_lags if dynamic_lags else [1, 2, 3]
+    lagged_data = {f'Lag_{lag}': price_data.shift(lag) for lag in lags}
+    lagged_data_df = pd.DataFrame(lagged_data).dropna()
+
+    X = lagged_data_df.values
+    y = price_data[lagged_data_df.index]
+
+    train_size = int(0.8 * len(X))
+    X_train, X_test = X[:train_size], X[train_size:]
+    y_train, y_test = y[:train_size], y[train_size:]
+
+    model = xgb.XGBRegressor(objective='reg:squarederror', max_depth=3, learning_rate=0.1, n_estimators=100)
     model.fit(X_train, y_train)
-    
-    # Predict future values
-    y_pred = model.predict(X_test)
-    future_preds = []
-    
-    # Iteratively predict for the forecast period
-    for _ in range(forecast_days):
-        last_known_data = [close_prices[-lag] for lag in dynamic_lags]
-        next_pred = model.predict(np.array([last_known_data]))[0]
-        future_preds.append(next_pred)
-        close_prices = np.append(close_prices, next_pred)
-    
-    return pd.Series(y_test, index=stock_data.index[-forecast_days:]), pd.Series(y_pred, index=stock_data.index[-forecast_days:]), future_preds
 
-# Run the main function
-if __name__ == "__main__":
+    y_pred = model.predict(X_test)
+
+    future_preds = []
+    last_values = X_test[-1].reshape(1, -1)
+    for _ in range(forecast_days):
+        next_pred = model.predict(last_values)[0]
+        future_preds.append(next_pred)
+        next_values = np.roll(last_values, shift=-1)
+        next_values[0, -1] = next_pred
+        last_values = next_values
+
+    return y_test, y_pred, future_preds
+
+if __name__ == '__main__':
     main()
